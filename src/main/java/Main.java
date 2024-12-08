417cef75149d95a3aec99241c7e29ee7765428ff
mamatthew <matthew.ma@alumni.ubc.ca>
2024-12-07 21:16:03 -0800

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,6 +17,17 @@ public class Main {
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
+
+          while (true) {
+            // Read input from client.
+            byte[] input = new byte[1024];
+            clientSocket.getInputStream().read(input);
+            String inputString = new String(input).trim();
+            System.out.println("Received: " + inputString);
+            clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            break;
+          }
+
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
