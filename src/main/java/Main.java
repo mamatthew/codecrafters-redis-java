3569630b6e8dddbd7d8429804303778f305a11f0
matth <mamatthe@students.cs.ubc.ca>
2024-12-14 11:27:46 -0800

    add port configuration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,15 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
-    private static final int port = 6379;
+    private static final int DEFAULT_PORT = 6379;
     private static final int THREAD_POOL_SIZE = 10;
     public static String rdbFilePath = null;
+    public static int customPort = -1;
 
     public static void main(String[] args){
           setup(args);
           ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
-
+          int port = customPort != -1 ? customPort : DEFAULT_PORT;
           try (ServerSocket serverSocket = new ServerSocket(port)) {
               serverSocket.setReuseAddress(true);
               while (true) {
@@ -29,6 +30,10 @@ public class Main {
     }
 
     private static void setup(String[] args) {
+        if (args.length == 2 && args[0].equals("--port")) {
+            customPort = Integer.parseInt(args[1]);
+            return;
+        }
         if (args.length != 4) {
             return;
         }
